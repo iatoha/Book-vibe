dfc7534dc566d549fb0ac1cb94c25188873206a9
iatoha <iatohaph@gmail.com>
2024-03-29 00:55:41 +0600

    again test again2

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes';
-// import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 
