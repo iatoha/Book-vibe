e60e48bd32d74ef023e6dd2c90d49e9e6aef65fe
iatoha <iatohaph@gmail.com>
2024-03-29 00:48:38 +0600

    again test

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
        <RouterProvider router={router} />
-       <ToastContainer></ToastContainer>
+       {/* <ToastContainer></ToastContainer> */}
   </React.StrictMode>,
 )
