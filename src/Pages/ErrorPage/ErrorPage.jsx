457397ede01d6f93adfb7dfe9b001d45a43ddf67
iatoha <iatohaph@gmail.com>
2024-03-28 23:40:32 +0600

    error page done

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -4,10 +4,10 @@ const ErrorPage = () => {
     return (
         <div className=" mx-auto lg:flex justify-center items-center h-screen">
             <div>
-            <h1 className=" text-7xl font-medium text-center ">Error 404</h1>
-            <p className="mt-5 text-3xl text-center">Sorry, We Misplaced That Page</p>
+            <h1 className=" text-7xl font-medium text-center ">Error 404...</h1>
+            <p className="mt-5 text-3xl text-center">You have been come in wrong page</p>
             <br />
-            <p className=" lg:w-[800px] text-center">Our digital librarian seems to have misplaced the page you requested. Stay with us, <br /> and  we'll help you rediscover it.</p>
+            <p className=" lg:w-[800px] text-center">.</p>
             <div className=" text-center flex justify-center py-8">
             <Link>
             <Button color="white">Home</Button></Link>
