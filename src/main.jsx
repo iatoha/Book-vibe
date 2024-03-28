54f0e043a06f1e59c8c70d5ed3b47410ba7b4c3f
iatoha <iatohaph@gmail.com>
2024-03-29 00:41:55 +0600

    toastify add again

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
