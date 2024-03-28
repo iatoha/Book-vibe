a359fe4f9316ee5ae99193dfef0ddbfe71fd801a
iatoha <iatohaph@gmail.com>
2024-03-28 23:41:48 +0600

    error changing

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
             <h1 className=" text-7xl font-medium text-center ">Error 404...</h1>
             <p className="mt-5 text-3xl text-center">You have been come in wrong page</p>
             <br />
-            <p className=" lg:w-[800px] text-center">.</p>
+            <p className=" lg:w-[800px] text-center">pls press the home button to go back</p>
             <div className=" text-center flex justify-center py-8">
             <Link>
             <Button color="white">Home</Button></Link>
