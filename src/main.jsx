23b7f6403cab31d612dbe6b79f62600559177ef3
iatoha <iatohaph@gmail.com>
2024-03-29 16:01:52 +0600

    trying

--- src/main.jsx
+++ src/main.jsx
@@ -12,4 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
        <RouterProvider router={router} />
        <ToastContainer></ToastContainer>
   </React.StrictMode>,
+  // abcsddd
 )
